d21fcbce2c527550f8f1d84634f62707b04d0514
Simon MacLean <sjd@oneape.xyz>
2023-09-25 03:16:33 -0700

    Restyle

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ fetch('https://api.github.com/users/SimonMacLean/repos')
     .then(response => response.json())
     .then(data => {
         const projectSection = document.getElementById('github-projects');
-        
+        const projectList = document.getElementById('project-list');
+
         data.forEach(project => {
-            const projectCard = document.createElement('div');
-            projectCard.classList.add('project-card');
+            const projectListItem = document.createElement('li');
+            projectListItem.textContent = `${project.name} - ${project.description}`;
+
+            const newTabIcon = document.createElement('i');
+            newTabIcon.className = 'fas fa-external-link-alt';
+            newTabIcon.style.marginLeft = '5px';
+
+            const iconLink = document.createElement('a');
+			iconLink.href = project.html_url;
+			iconLink.target = "_blank";
+			iconLink.className = 'subdued-link';
+			iconLink.appendChild(newTabIcon);
+
+            projectListItem.appendChild(iconLink);
             
-            const projectTitle = document.createElement('h3');
-            projectTitle.textContent = project.name;
-            
-            const projectLink = document.createElement('a');
-            projectLink.href = project.html_url;
-            projectLink.textContent = 'View on GitHub';
-            
-            projectCard.appendChild(projectTitle);
-            projectCard.appendChild(projectLink);
-            
-            projectSection.appendChild(projectCard);
+            projectList.appendChild(projectListItem);
         });
+
+        const githubLink = document.createElement('a');
+        githubLink.href = 'https://github.com/SimonMacLean';
+        githubLink.textContent = 'View All Projects on GitHub';
+        githubLink.className = 'subdued-link';
+
+        const githubLinkWrapper = document.createElement('div');
+        githubLinkWrapper.appendChild(githubLink);
+
+        projectSection.insertBefore(githubLinkWrapper, projectList);
     });
